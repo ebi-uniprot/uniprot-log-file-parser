583abac26eecce169d5eb8c216cd9dda00a2df84
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-04-17 15:28:16 +0100

    Add argument parsing; save json file based on incoming log directory and filename.

--- uniprot_log_file_parser.py
+++ uniprot_log_file_parser.py
@@ -3,7 +3,7 @@ import json
 from urllib import parse
 import get_non_browser_pattern
 from os import path
-
+import argparse
 
 non_browser_pattern = get_non_browser_pattern.get_non_browser_pattern()
 re_non_browser = re.compile(non_browser_pattern, re.IGNORECASE)
@@ -74,13 +74,24 @@ def write_to_json_file(obj, json_file_path):
         json.dump(obj, f, indent=4)
 
 
-def main():
-    log_file_path = '/nfs/public/rw/homes/tc_uni01/uuw-stats/logs/wp-p1m-cd/access_2019-03-17.log'
-    browser_requests = parse_log_file(log_file_path)
-    log_filename = path.basename(log_file_path)
-    json_file_path = path.join('/homes/dlrice/', log_filename)
-    write_to_json_file(browser_requests, json_file_path)
+def get_json_out_filename(log_file_path):
+    file_path, filename = path.split(log_file_path)
+    _, directory = path.split(file_path)
+    return f'{directory}__{filename}.json'
+
+
+def main(log_file_path, out_directory):
+    valid_browser_requests = parse_log_file(log_file_path)
+    json_out_filename = get_json_out_filename(log_file_path)
+    json_file_path = path.join(out_directory, json_out_filename)
+    write_to_json_file(valid_browser_requests, json_file_path)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('log_file_path', type=str,
+                        help='The path to log file to be parsed')
+    parser.add_argument('out_directory', type=str,
+                        help='The path to the directory for the resulting json file to be saved')
+    args = parser.parse_args()
+    main(args.log_file_path, args.out_directory)
