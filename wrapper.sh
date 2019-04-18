172a9fb3ad0431a60e57c3dafc91a1fb68bdc7b6
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-04-18 14:43:11 +0100

    Update log location in wrapper.sh.

--- wrapper.sh
+++ wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-LOGS=/homes/dlrice/parsed_logs/uuw-stats-logs.txt
+LOGS=/homes/dlrice/uniprot-log-file-parser/uuw-stats-logs.txt
 file=$(head -n $LSB_JOBINDEX $LOGS | tail -1)
 PARSER_PATH=/homes/dlrice/uniprot-log-file-parser/uniprot_log_file_parser.py
 OUT_DIR=/homes/dlrice/parsed_logs
