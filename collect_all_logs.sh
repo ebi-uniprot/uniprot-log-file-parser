d469052390e667cccdafc6928e129e9b6fdd6011
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-04-18 14:38:45 +0100

    Add collect_all_logs.sh script; update logs location in start_job_array.sh.

---
+++ collect_all_logs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find /nfs/public/rw/homes/tc_uni01/uuw-stats/logs/ -name *.log > uuw-stats-logs.txt
--- start_job_array.sh
+++ start_job_array.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-LOGS=/homes/dlrice/parsed_logs/uuw-stats-logs.txt
+LOGS=/homes/dlrice/uniprot-log-file-parser/uuw-stats-logs.txt
 n=$(wc -l < $LOGS)
 bsub -J"logparse[1-$n]" -o ~/out/logparse.o.%J-%I -e ~/error/logparse.e.%J-%I ./wrapper.sh
