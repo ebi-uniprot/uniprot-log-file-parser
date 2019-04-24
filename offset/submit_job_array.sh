93b38a72e72c5bb0feb7c30b1c566b312fd055ab
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-04-24 14:18:25 +0100

    Fix out/error directory.

--- offset/submit_job_array.sh
+++ offset/submit_job_array.sh
@@ -6,8 +6,8 @@ n=$(wc -l < $LOG_JSON_FILE_LIST)
 OUT_DIRECTORY=~/out/offset
 ERROR_DIRECTORY=~/error/offset
 RESULTS_DIRECTORY=~/parsed_logs/offset
-mkdir -p $OUT
-mkdir -p $ERROR
+mkdir -p $OUT_DIRECTORY
+mkdir -p $ERROR_DIRECTORY
 mkdir -p $RESULTS_DIRECTORY
 bsub -J"offset[1-$n]" \
 -o $OUT_DIRECTORY/o.%J-%I \
