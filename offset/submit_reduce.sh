4389753bf5667162650415a215fe1836a085bd7e
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-05-14 14:25:07 +0100

    Ammend offset/submit_reduce.sh to work with config.sh.

--- offset/submit_reduce.sh
+++ offset/submit_reduce.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-OUT_DIRECTORY=~/jobs/out/offset
-ERROR_DIRECTORY=~/jobs/error/offset
-OFFSET_COUNTS_DIRECTORY=~/parsed_logs/offset
-MERGED_PATH=~/parsed_logs/offset/merged.json
+source ../config.sh
 
 mkdir -p $OUT_DIRECTORY
 mkdir -p $ERROR_DIRECTORY
 
+MERGED_PATH=$OFFSET_DIRECTORY/merged.json
+
 bsub \
--o $OUT_DIRECTORY/reduce.o \
--e $ERROR_DIRECTORY/reduce.e \
-./reduce.py $OFFSET_COUNTS_DIRECTORY $MERGED_PATH
+-o $OUT_DIRECTORY/o \
+-e $ERROR_DIRECTORY/e \
+./reduce.py $OFFSET_DIRECTORY $MERGED_PATH
