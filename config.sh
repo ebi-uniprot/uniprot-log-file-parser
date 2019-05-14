5425e768dc94c2960043657b3dc82d11bbd16fbd
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-05-14 13:56:29 +0100

    Update offset/submit_map.sh to use config.sh.

--- config.sh
+++ config.sh
@@ -2,9 +2,11 @@
 LOG_PARSING_DIRECTORY=/net/isilonP/public/rw/homes/uni_adm/tmp/log_parsing/
 JSON_DIRECTORY=$LOG_PARSING_DIRECTORY/json
 COLUMNS_DIRECTORY=$LOG_PARSING_DIRECTORY/columns
+OFFSET_DIRECTORY=$LOG_PARSING_DIRECTORY/offset
 JOBS_DIRECTORY=~/jobs/
 DATETIME=$(date '+%Y.%m.%d-%H.%M.%S')
 OUT_DIRECTORY=$JOBS_DIRECTORY/$DATETIME/out
 ERROR_DIRECTORY=$JOBS_DIRECTORY/$DATETIME/error
 LOG_DIRECTORY=/nfs/public/rw/homes/tc_uni01/uuw-stats/logs/
 LOG_FILE_LIST=~/log_file_list.txt
+JSON_FILE_LIST=~/json_file_list.txt
--- offset/submit_map.sh
+++ offset/submit_map.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-LOG_JSON_DIRECTORY=~/parsed_logs
-OUT_DIRECTORY=~/jobs/out/offset
-ERROR_DIRECTORY=~/jobs/error/offset
-RESULTS_DIRECTORY=~/parsed_logs/offset
+source ../config.sh
 
-LOG_JSON_FILE_LIST=~/log_json_file_list.txt
-find $LOG_JSON_DIRECTORY -name *.log.json > $LOG_JSON_FILE_LIST
-n=$(wc -l < $LOG_JSON_FILE_LIST)
 mkdir -p $OUT_DIRECTORY
 mkdir -p $ERROR_DIRECTORY
-mkdir -p $RESULTS_DIRECTORY
+mkdir -p $OFFSET_DIRECTORY
+
+find $JSON_DIRECTORY -name *.log.json > $JSON_FILE_LIST
+n=$(wc -l < $JSON_FILE_LIST)
+
 bsub -J"offset[1-$n]" \
 -o $OUT_DIRECTORY/%J-%I \
 -e $ERROR_DIRECTORY/%J-%I \
-./map_wrapper.sh $LOG_JSON_FILE_LIST $RESULTS_DIRECTORY
+./map_wrapper.sh $JSON_FILE_LIST $OFFSET_DIRECTORY
