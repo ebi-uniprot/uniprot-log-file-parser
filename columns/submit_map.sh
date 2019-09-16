99869292b8e555b3a041ec3cfcf017ea5ac11b2b
Daniel Rice <dlrice@ebi.ac.uk>
2019-09-16 14:16:32 +0100

    Update columns to use config.sh

--- columns/submit_map.sh
+++ columns/submit_map.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-LOG_PARSING_DIRECTORY=/net/isilonP/public/rw/homes/uni_adm/tmp/log_parsing/
-LOG_JSON_DIRECTORY=$LOG_PARSING_DIRECTORY/json
-OUT_DIRECTORY=~/jobs/out/columns
-ERROR_DIRECTORY=~/jobs/error/columns
-LOG_JSON_DIRECTORY=$LOG_PARSING_DIRECTORY/columns
+source ../config.sh
 
-LOG_JSON_FILE_LIST=~/log_json_file_list.txt
-find $LOG_JSON_DIRECTORY -name *.log.json > $LOG_JSON_FILE_LIST
-n=$(wc -l < $LOG_JSON_FILE_LIST)
 mkdir -p $OUT_DIRECTORY
 mkdir -p $ERROR_DIRECTORY
-mkdir -p $RESULTS_DIRECTORY
-bsub -J"column[1-$n]" \
+mkdir -p $COLUMNS_DIRECTORY
+
+find $JSON_DIRECTORY -name *.log.json > $JSON_FILE_LIST
+n=$(wc -l < $JSON_FILE_LIST)
+
+bsub -J"columns[1-$n]" \
 -o $OUT_DIRECTORY/%J-%I \
 -e $ERROR_DIRECTORY/%J-%I \
-./map_wrapper.sh $LOG_JSON_FILE_LIST $RESULTS_DIRECTORY
+./map_wrapper.sh $JSON_FILE_LIST $OFFSET_DIRECTORY
