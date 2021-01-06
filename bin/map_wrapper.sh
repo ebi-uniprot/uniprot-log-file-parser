9c33e9ccf09bd85254f50b711dcf12bfe0b6b987
Daniel Rice <dlrice@ebi.ac.uk>
2021-01-06 18:25:08 +0000

    Add module to PYTHONPATH.

--- bin/map_wrapper.sh
+++ bin/map_wrapper.sh
@@ -2,5 +2,5 @@
 LOG_FILE_LIST=$1
 RESULTS_DIRECTORY=$2
 FILE=$(head -n $LSB_JOBINDEX $LOG_FILE_LIST | tail -1)
-./map.py $FILE $RESULTS_DIRECTORY
+PYTHONPATH=/homes/dlrice/uniprot-log-file-parser/map.py python3 -m uniprot_log_file_parser.map $FILE $RESULTS_DIRECTORY
 exit $?
