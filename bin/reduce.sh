6e88b4d24e8276b5f3c94697ab23d5d453fefd2d
Daniel Rice <dlrice@ebi.ac.uk>
2021-01-08 08:18:27 +0000

    Update analysis notebook.

--- bin/reduce.sh
+++ bin/reduce.sh
@@ -16,4 +16,3 @@ mkdir -p $REDUCE_DIRECTORY
 cat $MAP_DIRECTORY/*.bytes.csv > $REDUCE_DIRECTORY/bytes.csv
 cat $MAP_DIRECTORY/*.n-requests.csv > $REDUCE_DIRECTORY/n-requests.csv
 cat $MAP_DIRECTORY/*.parsed.csv > $REDUCE_DIRECTORY/parsed.csv
-cat $MAP_DIRECTORY/*.user-type.csv > $REDUCE_DIRECTORY/user-type.csv
