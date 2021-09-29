de4e3be7cbab4e9f5335f29f3350ff579f3d323b
Daniel Rice <dlrice@ebi.ac.uk>
2021-09-29 17:31:40 +0100

    Use 3GB for map jobs.

--- bin/submit_map.sh
+++ bin/submit_map.sh
@@ -5,13 +5,17 @@ mkdir -p $OUT_DIRECTORY
 mkdir -p $ERROR_DIRECTORY
 mkdir -p $MAP_DIRECTORY
 
-find $LOG_DIRECTORY -name *2020*.log > $LOG_FILE_LIST
+find $LOG_DIRECTORY -name *.log > $LOG_FILE_LIST
 # Or use the following line for testing
 # find $LOG_DIRECTORY -name *.log | head -n 10 > $LOG_FILE_LIST 
 n=$(wc -l < $LOG_FILE_LIST)
 
+mem=3000
+
 bsub \
 -J"parse[1-$n]" \
+-M $mem \
+-R"select[mem>$mem] rusage[mem=$mem] span[hosts=1]" \
 -o $OUT_DIRECTORY/%J-%I \
 -e $ERROR_DIRECTORY/%J-%I \
 ./map_wrapper.sh $LOG_FILE_LIST $MAP_DIRECTORY
