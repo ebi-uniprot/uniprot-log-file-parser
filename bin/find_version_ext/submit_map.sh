bae9cf0ec446ffd690f6449b7ee0ecc25a78cd64
Daniel Rice <dlrice@ebi.ac.uk>
2021-09-30 12:40:55 +0100

    Fix file extension in find.

--- bin/find_version_ext/submit_map.sh
+++ bin/find_version_ext/submit_map.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-CWD=$1
+CWD=/nfs/public/rw/homes/uni_adm/tmp/log_parsing/parsed-no-bots
 EXT_DIRECTORY=$CWD/version-ext
 OUT_DIRECTORY=$EXT_DIRECTORY/out
 ERROR_DIRECTORY=$EXT_DIRECTORY/error
-
-#/nfs/public/rw/homes/uni_adm/tmp/log_parsing/parsed-no-bots
 
 mkdir -p $EXT_DIRECTORY
 mkdir -p $OUT_DIRECTORY
@@ -14,11 +12,11 @@ mkdir -p $ERROR_DIRECTORY
 PARSED_DIRECTORY=$CWD/parsed-by-date
 FILE_LIST=$EXT_DIRECTORY/file_list.txt
 
-find $PARSED_DIRECTORY -name 2020*.log > $FILE_LIST
+find $PARSED_DIRECTORY -name 2020*.csv > $FILE_LIST
 n=$(wc -l < $FILE_LIST)
 
 bsub \
--J"parse[1-$n]" \
+-J"ext[1-$n]" \
 -o $OUT_DIRECTORY/%J-%I \
 -e $ERROR_DIRECTORY/%J-%I \
 ./map_wrapper.sh $FILE_LIST 
