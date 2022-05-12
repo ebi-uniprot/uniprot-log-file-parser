3bb998895e5dfa95963c69ab648826e579fe620d
Daniel Rice <dlrice@ebi.ac.uk>
2022-05-12 22:35:40 +0100

    Update config.

--- bin/config.sh
+++ bin/config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DATETIME=$(date '+%Y.%m.%d-%H.%M.%S')
-LOG_PARSING_DIRECTORY=/net/isilonP/public/rw/homes/uni_adm/tmp/log_parsing/$DATETIME
+LOG_PARSING_DIRECTORY=/net/isilonP/public/rw/homes/uni_adm/tmp/log_parsing/2020-10/results
 MAP_DIRECTORY=$LOG_PARSING_DIRECTORY/map
 OUT_DIRECTORY=$LOG_PARSING_DIRECTORY/out
 ERROR_DIRECTORY=$LOG_PARSING_DIRECTORY/error
 REDUCE_DIRECTORY=$LOG_PARSING_DIRECTORY/reduce
 LOG_FILE_LIST=$LOG_PARSING_DIRECTORY/log_file_list.txt
-LOG_DIRECTORY=/nfs/public/rw/homes/tc_uni01/uuw-stats/logs/
+LOG_DIRECTORY=/net/isilonP/public/rw/homes/uni_adm/tmp/log_parsing/2020-10/logs
