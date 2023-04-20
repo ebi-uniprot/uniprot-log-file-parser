3dbedca189b93ed8ba7349d2e313a1a483607e59
Daniel Rice <dlrice@ebi.ac.uk>
2023-04-20 11:25:40 +0100

    Clean up.

--- prepare_and_insert_logs_wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-/hps/nobackup/martin/uniprot/users/dlrice/clickhouse/clickhouse-common-static-22.12.1.1752/usr/bin/clickhouse server --daemon &
-./prepare_and_insert_logs.py
-./clickhouse stop
-exit $?
